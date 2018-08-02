17e0e23fe4a2cbd65e735bc624afb46affccfec7
Danny Marshall <35569463+Dharoldm@users.noreply.github.com>
2018-08-01 20:07:15 -0400

    Work
    Finished everything I need to add to subtract now just bug testing. Currently, can't have the ending month be after the starting month.

--- Date.py
+++ Date.py
@@ -1,5 +1,6 @@
 from LinkedList import LinkedList, HashListNode
 
+
 class Date(object):
     def __init__(self, day, month, year):
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -21,37 +22,118 @@ class Date(object):
         return f"{self.month_str} {self.day}, {self.year}"
 
     def subtraction(self, other):
-        curr_year = self.year
-        goal_year = other.year
-        curr_month = self.month
-        goal_month = other.month
-        days = 0
-        month = self.calendar.head
+        """
+        This finds the time between the two days given. Preferably, this method is called on the earlier date.
+        :param other: The other date
+        :return: The days between the two dates
+        """
+        curr_year = self.year  # The year that is being started from
+        goal_year = other.year  # The end year of this length of time being figured out
+        curr_month = self.month  # The starting month
+        goal_month = other.month  # Ending month
+        curr_day = self.day  # Starting day
+        goal_day = other.day  # Ending day
+        days = 0  # Days between the two dates
+        month = self.calendar.head  # Linked list of every month with their days
+        leap = False  # if the year is a leap year this is true
+        # I'm writing up here that leap years have stupid math asociated with them that'll make this code uglier than it
+        # is
         while month.key is not self.month_str:
             month = month.next
-
-        if abs(curr_year-goal_year) > 0:
-           while goal_year - curr_year > 1:
-               curr_year += 1
-               if curr_year%4 == 0:
-                   days += 366
-               else:
-                   days += 365
-           if curr_month < goal_month:
-               assert isinstance(goal_month, int)
-               curr_year += 1
-               if curr_year % 4 == 0:
-                   days += 366
-               else:
-                   days += 365
-               while curr_month < goal_month:
-                   days+= month.value
-                   month = month.next
-                   curr_month += 1
+        if curr_year != goal_year:
+            while goal_year - curr_year > 1:
+                curr_year += 1
+                if curr_year%4 == 0:
+                    if curr_year%100 != 0:
+                        days += 366
+                    elif curr_year%400 == 0:
+                        days += 366
+                    else:
+                        days += 365
+                else:
+                    days += 365
+            if curr_month < goal_month:
+                assert isinstance(goal_month, int)
+                curr_year += 1
+                if curr_year % 4 == 0:
+                    if curr_year % 100 != 0:
+                        days += 366
+                    elif curr_year % 400 == 0:
+                        days += 366
+                    else:
+                        days += 365
+                else:
+                    days += 365
+                if curr_year % 4 == 0:
+                    if curr_year % 100 != 0:
+                        leap = True
+                    elif curr_year % 400 == 0:
+                        leap = True
+                    else:
+                        leap = False
+                while curr_month < goal_month:
+                    if leap is True and month.key == "Feb":
+                        days += 29
+                        month = month.next
+                        curr_month += 1
+                    else:
+                        days += month.value
+                        month = month.next
+                        curr_month += 1
+        while month.next.key is not other.month_str:
+            if curr_year % 4 == 0:
+                if curr_year % 100 != 0:
+                    leap = True
+                elif curr_year % 400 == 0:
+                    leap = True
+                else:
+                    leap = False
+            if leap is True and month.key == "Feb":
+                days += 29
+                month = month.next
+                curr_month += 1
+            else:
+                days += month.value
+                month = month.next
+                curr_month += 1
+            if curr_month == 13:
+                curr_month = 1
+                curr_year += 1
+            leap = False
+        if curr_day < goal_day:
+            if leap is True and month.key == "Feb":
+                days += 29
+                month = month.next
+                curr_month += 1
+            else:
+                days += month.value
+                month = month.next
+                curr_month += 1
+            if curr_month == 13:
+                curr_month = 1
+                curr_year += 1
+                leap = False
+        else:
+            if leap is True and month.key == "Feb":
+                days += 29 - curr_day + 1
+                month = month.next
+                curr_month += 1
+                curr_day = 1
+            else:
+                days += month.value - curr_day + 1
+                month = month.next
+                curr_month += 1
+                curr_day = 1
+            if curr_month == 13:
+                curr_month = 1
+                curr_year += 1
+                leap = False
+        if curr_day < goal_day:
+            days += goal_day-curr_day
         return days
 
 
-test = Date(1,"Feb",-38)
-that = Date(1,"Aug",2018)
+test = Date(1, "Mar", 2002)
+that = Date(1, "Apr", 2205)
 print(test)
 print(test.subtraction(that))
